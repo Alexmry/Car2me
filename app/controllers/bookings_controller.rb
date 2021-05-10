4d39a7ac65ecb9d73e9f93489861a58ee5b9e77b
felix habert <uclqfbf@ucl.ac.uk>
2021-05-10 12:40:31 +0100

    added navbar + booking card

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,43 @@
 class BookingsController < ApplicationController
+    before_action :set_booking, only: [:show, :edit, :destroy]
+
     def index
         @bookings = Booking.all
     end
+
     def create
+        @booking = Booking.new(booking_params)
+        if @booking.save 
+            redirect_to bookings_path
+        else
+            render :new 
+        end
     end
+
     def new
+        @booking = Booking.new()
     end
+
     def destroy 
     end
+
     def update 
     end
+
     def edit 
     end
-    def show 
+
+    def show
     end
+
+    private
+
+    def booking_params
+        params.require(:bookings).permit(:confirmed, :car_id, :user_id)
+    end
+
+    def set_booking
+        @booking = Booking.find(params[:id])
+    end
+    
 end
