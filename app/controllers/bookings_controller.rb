f242dc221b066f22dd1b17880a394bf4c90d964f
Alexandre Morey <alexander006_9@hotmail.com>
2021-05-12 17:27:45 +0100

    booking btn and  feature updated

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,58 +1,60 @@
 class BookingsController < ApplicationController
-    before_action :set_booking, only: [:show, :edit, :destroy]
+  before_action :set_booking, only: [:show, :edit, :destroy]
 
-    def index
-        @bookings = Booking.where(user_id: current_user.id)
-        @cars = find_cars() 
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+    @cars = find_cars()
+  end
+
+  def new
+    @booking = Booking.new()
+  end
+
+  def create
+    @booking = Booking.new()
+    @booking.user_id = current_user.id
+    @booking.confirmed = true
+    @booking.car_id = params[:car_id]
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render 'cars/show'
     end
+  end
 
-    def create
-        @booking = Booking.new(booking_params)
-        if @booking.save 
-            redirect_to bookings_path
-        else
-            render :new 
-        end
+  def destroy
+   @booking.destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+    @car = Car.where(id: @booking.car_id)
+    @client = User.where(id: @booking.car_id)
+  end
+
+  private
+
+  def booking_params
+    # params.require(:bookings).permit(:confirmed, :car_id, :user_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def find_cars
+    cars = []
+    @bookings.each do |booking|
+      if booking.user_id == current_user
+        cars << booking.car_id
+      end
     end
-
-    def new
-        @booking = Booking.new()
-    end
-
-    def destroy
-        @booking.destroy
-    end
-
-    def update 
-    end
-
-    def edit
-    end
-
-    def show
-        @car = Car.where(id: @booking.car_id)
-        @client = User.where(id: @booking.car_id)
-    end
-
-    private
-
-    def booking_params
-        params.require(:bookings).permit(:confirmed, :car_id, :user_id)
-    end
-
-    def set_booking
-        @booking = Booking.find(params[:id])
-    end
-
-    def find_cars
-        cars = []
-        @bookings.each do |booking|
-            if booking.user_id = current_user
-                cars << booking.car_id
-            end
-        end
-        cars.map { |car_id| Car.find(car_id) }
-        return cars
-    end
-    
+    cars.map { |car_id| Car.find(car_id) }
+    return cars
+  end
 end
