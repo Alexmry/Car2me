250ebee904d8a865a8f43c279cf3e42bdd485ed6
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-05-18 21:08:30 +0100

    map&search complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @cars = Car.all
-
+    #@cars = Car.all no need as we have it below if query is not present
+     if params[:query].present?
+    @cars = Car.search_by_model_and_address(params[:query])
+    else
+      @cars = Car.all
+    end
     @markers = @cars.geocoded.map do |car|
       {
         lat: car.latitude,
@@ -11,3 +15,5 @@ class PagesController < ApplicationController
     end
   end
 end
+
+#car.all on line 10 to display all cars rather than the search.
