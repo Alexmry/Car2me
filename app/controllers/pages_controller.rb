5b5f2a779830cf58635c54f23537940a0aec78bf
Alexandre Morey <alexander006_9@hotmail.com>
2021-05-18 10:00:00 +0100

    added map to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @cars = Car.all
+
+    @markers = @cars.geocoded.map do |car|
+      {
+        lat: car.latitude,
+        lng: car.longitude
+      }
+    end
   end
 end
