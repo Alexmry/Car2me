916b4e176798b876f0771cbdc0574658f732a884
felix habert <uclqfbf@ucl.ac.uk>
2021-05-20 22:31:34 +0100

    adding flatpckr on show car

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_134549) do
+ActiveRecord::Schema.define(version: 2021_05_20_204905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2021_05_15_134549) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "starts_at"
+    t.string "ends_at"
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
